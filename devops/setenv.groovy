8139f80305678bc6d4d68d4524e8002237e59d70
sharmavish <vish04.sharma@gmail.com>
2020-01-25 21:20:29 +0530

    Update setenv.groovy

--- devops/setenv.groovy
+++ devops/setenv.groovy
@@ -53,8 +53,8 @@ zapfile.renameTo( new File('./zap') )
 def sonarfile = new File('sonar-scanner-3.3.0.1492')
 sonarfile.renameTo( new File('sonar') )
 
-def perm = "sudo pip install zapcli && sudo chmod -R 777 sonar ."
-def permproc = perm.execute()
-permproc.waitFor()              
-println "Perm Process exit code: ${permproc.exitValue()}"
+def cmd = "sudo pip install zapcli && sudo chmod -R 777 sonar ."
+def cmdproc = cmd.execute()
+cmdproc.waitFor()              
+println "Perm Process exit code: ${cmdproc.exitValue()}"
 
