2be5de936686f8bac8d3f7955bc672a463fe35fc
sharmavish <vish04.sharma@gmail.com>
2020-01-25 20:22:14 +0530

    Update setenv.groovy

--- devops/setenv.groovy
+++ devops/setenv.groovy
@@ -1,17 +1,17 @@
 // Install required packages.
 
-def command = "sudo apt install nodejs openjdk-8-jre unzip"
-def command1 = "sudo npm install -g @sanity/cli netlify-cli"
-def proc = command.execute()
-def proc1 = command1.execute()
-proc.waitFor()              
-proc1.waitFor()
-println "Process exit code command: ${proc.exitValue()}"
-println "Process exit code command1: ${proc1.exitValue()}"
-println "Std Err command : ${proc.err.text}"
-println "Std Out command : ${proc.in.text}" 
-println "Std Err command1: ${proc1.err.text}"
-println "Std Out command1: ${proc1.in.text}" 
+def apt = "sudo apt install nodejs openjdk-8-jre unzip"
+def npm = "sudo npm install -g @sanity/cli netlify-cli && sudo pip install zapcli"
+def aptproc = apt.execute()
+def npmproc = npm.execute()
+aptproc.waitFor()              
+npmproc.waitFor()
+println "Apt Process exit code: ${aptproc.exitValue()}"
+println "Npm Process exit code: ${npmproc.exitValue()}"
+println "Std Err command : ${aptproc.err.text}"
+println "Std Out command : ${aptproc.in.text}" 
+println "Std Err command1: ${npmproc.err.text}"
+println "Std Out command1: ${npmproc.in.text}" 
 
 // Download Sonar scanner.
 def sonarscanner = "wget  -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip"
@@ -31,33 +31,27 @@ scanner.unzip(  src:"sonar-scanner-cli-3.3.0.1492.zip",
             dest:"./",
             overwrite:"false" )
 
-// Unzip the sonar zip
+// Unarchive the ZAP proxy 
 def zapproxy = new AntBuilder()   
 zapproxy.untar(  src:"ZAP_2.9.0_Linux.tar.gz",
             dest:"./",
             compression:"gzip",
             overwrite:"false" )
 
+// Delete the ZAP and sonar scanner zip files 
  def zapgz = new File('./ZAP_2.9.0_Linux.tar.gz')   
  def sonarzip = new File('./sonar-scanner-cli-3.3.0.1492.zip')
  zapgz.delete()
  sonarzip.delete()
   
-
+// Rename the ZAP and sonar-scanner
 def zapfile = new File('./ZAP_2.9.0')
 zapfile.renameTo( new File('./zap') )
-
 def sonarfile = new File('sonar-scanner-3.3.0.1492')
 sonarfile.renameTo( new File('sonar') )
 
-// - >*
-// && rm -rf sonar-scanner-cli-3.3.0.1492.zip && sudo mv sonar-scanner-3.3.0.1492 sonar \
+def perm = "sudo chmod -R 777 ./"
+def permproc = sonarscanner.execute()
+perm.waitFor()              
+println "Perm Process exit code: ${perm.exitValue()}"
 // && sudo chmod -R 777 sonar ${GITHUB_WORKSPACE}
-
-// ## Install zap
- // wget -q https://github.com/zaproxy/zaproxy/releases/download/v2.9.0/ZAP_2.9.0_Linux.tar.gz > /dev/null \
-// && sudo pip install zapcli \
-// && sudo tar xzf ZAP_2.9.0_Linux.tar.gz > /dev/null && sudo mv ZAP_2.9.0 zap \
-// && sudo rm -rf ZAP_2.9.0_Linux.tar.gz 
-
-// 
