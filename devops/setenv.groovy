b3b553e407feadd0deed0354598f175391ef4ccb
sharmavish <vish04.sharma@gmail.com>
2020-01-25 14:35:43 +0530

    Update setenv.groovy

--- devops/setenv.groovy
+++ devops/setenv.groovy
@@ -1,7 +1,8 @@
-def command = "sudo apt update && sudo apt install nodejs openjdk-8-jre unzip && sudo npm install -g \@sanity/cli netlify-cli"
+def command = "sudo apt install nodejs openjdk-8-jre unzip"
+def command1 = "sudo npm install -g @sanity/cli netlify-cli"
 def proc = command.execute()
+def proc1 = command1.execute()
 proc.waitFor()              
-
+proc1.waitFor()
 println "Process exit code: ${proc.exitValue()}"
-println "Std Err: ${proc.err.text}"
-println "Std Out: ${proc.in.text}"
+println "Process exit code: ${proc1.exitValue()}"
