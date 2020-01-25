58b47f69a8212cd2565ae6c231646f320aec6679
sharmavish <vish04.sharma@gmail.com>
2020-01-25 15:10:04 +0530

    Update setenv.groovy

--- devops/setenv.groovy
+++ devops/setenv.groovy
@@ -1,3 +1,5 @@
+// Install required packages.
+
 def command = "sudo apt install nodejs openjdk-8-jre unzip"
 def command1 = "sudo npm install -g @sanity/cli netlify-cli"
 def proc = command.execute()
@@ -10,3 +12,10 @@ println "Std Err command : ${proc.err.text}"
 println "Std Out command : ${proc.in.text}" 
 println "Std Err command1: ${proc1.err.text}"
 println "Std Out command1: ${proc1.in.text}" 
+
+
+new File(System.getenv("GITHUB_WORKSPACE")).withOutputStream { out ->
+    new URL('https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip').eachByte { soanrout ->
+        out.write(sonarout)
+    }
+
