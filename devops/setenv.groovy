2cd1b2732a07a84041f201001eafbf193e6d86e8
sharmavish <vish04.sharma@gmail.com>
2020-01-25 15:51:46 +0530

    Update setenv.groovy

--- devops/setenv.groovy
+++ devops/setenv.groovy
@@ -22,5 +22,5 @@ println "sonar Process exit code: ${sonar.exitValue()}"
 // Unzip the sonar zip
 def ant = new AntBuilder()   
 ant.unzip(  src:"sonar-scanner-cli-3.3.0.1492.zip",
-            dest:"System.getenv("GITHUB_WORKSPACE")",
+            dest:System.getenv("GITHUB_WORKSPACE"),
             overwrite:"false" )
