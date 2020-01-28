1eed2be4967a6793a79be272133e0c11aaba0424
sharmavish <vish04.sharma@gmail.com>
2020-01-28 22:11:17 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,3 +1,4 @@
 #!/usr/bin/env groovy
+def test = System.getenv("PROJECT_NAME")
 def sanity = new AntBuilder()   
-sanity.replace(file: "./sanity.json", token: "project_name", value: "System.getenv("PROJECT_NAME")")
+sanity.replace(file: "./sanity.json", token: "project_name", value: "test")
