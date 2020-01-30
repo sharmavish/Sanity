3e323353409282f92411f352f579aeefdd764e19
sharmavish <vish04.sharma@gmail.com>
2020-01-30 22:28:18 +0530

    Update install.py

--- devops/install.py
+++ devops/install.py
@@ -2,7 +2,9 @@
 import os
 from zipfile import ZipFile
 from urllib import urlretrieve
-from tempfile import mktemp
+import tarfile
+import urllib
+
 os.system("sudo apt update && sudo apt install nodejs openjdk-8-jre && sudo npm install -g @sanity/cli netlify-cli")
 
 filename ='sonar-scanner-cli-3.3.0.1492.zip'
@@ -14,5 +16,14 @@ thefile.extractall(destDir)
 thefile.close()
 
 os.rename('./devops/sonar-scanner-3.3.0.1492','./devops/sonar')
+
+url = 'https://github.com/zaproxy/zaproxy/releases/download/v2.9.0/ZAP_2.9.0_Linux.tar.gz'
+
+file_tmp = urllib.urlretrieve(url, filename=None)[0]
+base_name = os.path.basename(url)
+
+file_name, file_extension = os.path.splitext(base_name)
+tar = tarfile.open(file_tmp)
+tar.extractall(file_name)
 
 
