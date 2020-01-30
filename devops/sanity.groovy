b23b081cb2b2db7de05d6909d02d3796a789b6d0
sharmavish <vish04.sharma@gmail.com>
2020-01-30 17:49:18 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,5 +1,13 @@
-#!/usr/bin/env groovy 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-Files.createDirectories(Paths.get("/home/runner/.config/sanity"));
+def cmd = "sudo mkdir -p /home/runner/.config/sanity/"
+def proc = cmd.execute()
+proc.waitFor()              
+println "Process exit code: ${proc.exitValue()}"
+
+def cp = "sudo cp -p devops/config.json /home/runner/.config/sanity/"
+def cpproc = cp.execute()
+cpproc.waitFor()              
+println "Process exit code: ${cpproc.exitValue()}"
+
+String contents = new File( '/home/runner/.config/sanity/config.json' ).getText( 'UTF-8' ) 
+contents = contents.replaceAll( 'SANITY_AUTH_TOKEN', ${{ secrets.SANITY_AUTH_TOKEN }} )
 
