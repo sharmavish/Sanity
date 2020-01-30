9d3d84144ee579b13a2a3798c4ab68c6fa7df751
sharmavish <vish04.sharma@gmail.com>
2020-01-30 17:10:46 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -5,3 +5,8 @@ def file = new File("devops/config.json")
 def newFile = new File("/home/runner/.config/sanity/")
 Files.copy(file.toPath(), newFile.toPath())
 
+def cmd ="sudo sed -i "s|SANITY_AUTH_TOKEN|`echo ${{ secrets.SANITY_AUTH_TOKEN }}`|g" /home/runner/.config/sanity/config.json"
+def proc = cmd.execute()
+proc.waitFor()
+println "Process exit code: ${proc.exitValue()}"
+
