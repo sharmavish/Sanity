d0a0fdd1c9a7cbed1da3c99cbe0c9e14a70ce29c
sharmavish <vish04.sharma@gmail.com>
2020-01-30 21:50:20 +0530

    Update install.py

--- devops/install.py
+++ devops/install.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-import zipfile, urllib.request, shutil
+import zipfile, urllib.requests, shutil
 from envs import env
 os.system("sudo apt update && sudo apt install nodejs openjdk-8-jre && sudo npm install -g @sanity/cli netlify-cli && sudo pip install requests")
 
