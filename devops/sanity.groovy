679a452a4e69618716c5bd7871a21141e2814487
sharmavish <vish04.sharma@gmail.com>
2020-01-30 11:57:37 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy 
+def PROJECT_NAME = Syetm.getenv("PROJECT_NAME")
+def PROJECT_ID = Syetm.getenv("PROJECT_ID")
+def DATEAET_NAME = Syetm.getenv("DATEAET_NAME")
 def sedcmd = "sudo sed -i \"s|projectname|$PROJECT_NAME|g;s|projectid|$PROJECT_ID|g;s|datasetname|$DATEAET_NAME|g\" sanity.json"
 def sedcmdproc = sedcmd.execute()           
 sedcmdproc.waitFor()
