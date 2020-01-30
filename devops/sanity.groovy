9b9530d308883e35f572bd303c1ea9c96f65f04d
sharmavish <vish04.sharma@gmail.com>
2020-01-30 10:20:18 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,5 +1,4 @@
-#!/usr/bin/env groovy
- def project = System.getenv("PROJECT_NAME")
+#!/usr/bin/env groovy 
  def file = new File('./sanity.json')
- def newConfig = file.text.replace('projectname', 'project')
+ def newConfig = file.text.replace('projectname', System.getenv("PROJECT_NAME"))
  file.text = newConfig
