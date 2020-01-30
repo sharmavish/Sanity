23387c8e17439cc09575e7bc1cf338636572c5ca
sharmavish <vish04.sharma@gmail.com>
2020-01-30 12:16:56 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy 
-def PROJECT_NAME = System.getenv("PROJECT_NAME")
-def PROJECT_ID = System.getenv("PROJECT_ID")
-def DATEAET_NAME = System.getenv("DATEAET_NAME")
-def sedcmd = "sudo sed -i \"s|projectname|$PROJECT_NAME|g;s|projectid|$PROJECT_ID|g;s|datasetname|$DATEAET_NAME|g\" sanity.json"
-def sedcmdproc = sedcmd.execute()           
-sedcmdproc.waitFor()
-println "apt Process exit code: ${sedcmdproc.exitValue()}"
-println "Std Err apt : ${sedcmdproc.err.text}"
-
+#def PROJECT_NAME = System.getenv("PROJECT_NAME")
+#def PROJECT_ID = System.getenv("PROJECT_ID")
+#def DATEAET_NAME = System.getenv("DATEAET_NAME")
+def file = new File('sanity.json')
+def newConfig = file.text.replace('projectname', '${PROJECT_NAME}')
+file.text = newConfig
