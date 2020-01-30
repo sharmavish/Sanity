078b5d2b870fec1e1fdb8d0f89a3ad0e96fc0dff
sharmavish <vish04.sharma@gmail.com>
2020-01-30 21:45:50 +0530

    Update install.py

--- devops/install.py
+++ devops/install.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import os
-import requests, zipfile, io
+import zipfile, urllib.request, shutil
 from envs import env
 os.system("sudo apt update && sudo apt install nodejs openjdk-8-jre && sudo npm install -g @sanity/cli netlify-cli")
-url_req = requests.get('https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip')
-zip = zipfile.ZipFile(io.BytesIO(url_req.content))
-zip.extractall()
+
+
+
+url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip'
+file_name = 'sonar-scanner-cli-3.3.0.1492.zip'
+with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+    with zipfile.ZipFile(file_name) as zf:
+        zf.extractall("./devops/")
 
 
