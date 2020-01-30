d4f79d5103558ba98c07f2711f802a5f91c9ce1e
sharmavish <vish04.sharma@gmail.com>
2020-01-30 17:02:09 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy 
-fileText = ./sanity.json;
-fileText = fileText.replace(projectname, System.getenv("PROJECT_NAME"))
-file.write(fileText)
+ def subdir = new File('/home/runner/.config/sanity/')
+      subdir.mkdir()
+
