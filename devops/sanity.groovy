de12a8d4bc6da9f060513b6eb932f96169fa9859
sharmavish <vish04.sharma@gmail.com>
2020-01-30 12:30:46 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -2,5 +2,5 @@
 def project_name = "projectname"
 def project = "System.getenv("PROJECT_NAME")"
 fileText = ./sanity.json;
-fileText = fileText.replaceAll(project_name, project)
+fileText = fileText.replace(project_name, project)
 file.write(fileText)
