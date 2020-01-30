7a1bb10fd9562c29a2111b500869cab72244d2f5
sharmavish <vish04.sharma@gmail.com>
2020-01-30 11:46:47 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy 
-def env = System.getenv()
-def sedcmd = "sudo sed -i \"s|projectname|env\[\'PROJECT_NAME\'\]|g;s|projectid|env\[\'PROJECT_ID\'\]|g;s|datasetname|env\[\'DATEAET_NAME\'\]|g\" sanity.json"
+def sedcmd = " sudo sed -i "s|projectname|$PROJECT_NAME|g;s|projectid|$PROJECT_ID|g;s|datasetname|$DATEAET_NAME|g" sanity.json"
 def sedcmdproc = sedcmd.execute()           
 sedcmdproc.waitFor()
 println "apt Process exit code: ${sedcmdproc.exitValue()}"
