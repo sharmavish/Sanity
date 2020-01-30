efba76645fca1c101b049860971e9efd658a771f
sharmavish <vish04.sharma@gmail.com>
2020-01-30 21:29:07 +0530

    Update install.py

--- devops/install.py
+++ devops/install.py
@@ -7,10 +7,10 @@ from zipfile import ZipFile
 os.system("sudo apt update && sudo apt install nodejs openjdk-8-jre && sudo npm install -g @sanity/cli netlify-cli")
 zipurl = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip'
 zipresp = urlopen(zipurl)
-tempzip = open(./sonar-scanner-cli-3.3.0.1492.zip", "wb")
+tempzip = open(sonar-scanner-cli-3.3.0.1492.zip", "wb")
 tempzip.write(zipresp.read())
 tempzip.close()
-zf = ZipFile("./sonar-scanner-cli-3.3.0.1492.zip")
+zf = ZipFile("sonar-scanner-cli-3.3.0.1492.zip")
 zf.extractall(path = '<extraction_path>')
 zf.close()
                
