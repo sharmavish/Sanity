bf375813bac16115eec3a38408daaef6a1d487e6
sharmavish <vish04.sharma@gmail.com>
2020-01-30 17:07:40 +0530

    Update sanity.groovy

--- devops/sanity.groovy
+++ devops/sanity.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy 
  def subdir = new File('/home/runner/.config/sanity/')
       subdir.mkdir()
+def file = new File("devops/config.json")
+def newFile = new File("/home/runner/.config/sanity/")
+Files.copy(file.toPath(), newFile.toPath())
 
