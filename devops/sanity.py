554fb7692808ce20820b83dbb7a4f99cc3a99857
sharmavish <vish04.sharma@gmail.com>
2020-01-31 14:53:09 +0530

    Update sanity.py

--- devops/sanity.py
+++ devops/sanity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+import os, sys
 fin = open("sanity.json", "rt")
 data = fin.read()
 data = data.replace('projectname', os.getenv('PROJECT_NAME'))
@@ -10,3 +10,16 @@ fin.close()
 fin = open("sanity.json", "wt")
 fin.write(data)
 fin.close()
+
+###### FOR Auth token ####
+path = "/home/runner/.config/sanity"
+os.makedirs( path, 0777 );
+os.system('cp -p ./devops/config.json /home/runner/.config/sanity/')
+con = open("/home/runner/.config/sanity/config.json", "rt")
+sanitydata = con.read()
+sanitydata= sanitydata.replace('SANITY_AUTH_TOKEN', os.getenv('SANITY_AUTH_TOKEN'))
+con.close()
+con = open("/home/runner/.config/sanity/config.json", "wt")
+con.write(sanitydata)
+con.close()
+
