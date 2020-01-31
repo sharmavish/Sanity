1528c96d78dea472cbf4d7c02460d63d938a0df8
sharmavish <vish04.sharma@gmail.com>
2020-01-31 12:45:40 +0530

    Update sanity.py

--- devops/sanity.py
+++ devops/sanity.py
@@ -1,8 +1,4 @@
 # sudo sed -i "s|projectname|$PROJECT_NAME|g;s|projectid|$PROJECT_ID|g;s|datasetname|$DATEAET_NAME|g" sanity.json 
 #!/usr/bin/env python
 import os
-import subprocess
-PROJECT = os.environ['PROJECT_NAME']
-ID = os.environ['PROJECT_ID']
-DATESET = os.environ['DATEAET_NAME']
-os.system("sudo sed -i 's|projectname|$PROJECT|g;s|projectid|$ID|g;s|datasetname|$DATEAET|g' sanity.json")
+os.system("sudo sed -i "s|projectname|os.getenv('PROJECT_NAME')|g;s|projectid|os.getenv('PROJECT_IS')|g;s|datasetname|os.getenv('DATEAET_NAME')|g" sanity.json")
